42b9973e140f211c63206c50043de6f818b9c18c
thisyourei <60913697+thisyourei@users.noreply.github.com>
2021-05-02 16:47:48 -0400

    files

---
+++ Default.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Oracle.DataAccess.Client;
+
+namespace pruebitanetframework
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        OracleConnection conexion = new OracleConnection("DATA SOURCE = ORCL ; PASSWORD = Lala1234 ; USER ID = Administrador");
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            conexion.Open();
+            OracleCommand comando = new OracleCommand("SELECT * FROM PERSONA WHERE USUARIO = :usuario AND CLAVE = :clave", conexion);
+
+            comando.Parameters.Add(":usuario", txtUsuario.Text);
+            comando.Parameters.Add(":clave", txtClave.Text);
+
+            OracleDataReader lector = comando.ExecuteReader();
+
+            if (lector.Read())
+            {
+                if (lector["ROL"].ToString()=="ADMIN")
+                {
+                    Server.Transfer("HomeAdmin.aspx");
+                    conexion.Close();
+                }
+
+                if (lector["ROL"].ToString() == "NORMAL")
+                {
+                    Server.Transfer("HomeNormal.aspx");
+                    conexion.Close();
+                }
+            }
+            else
+            {
+
+            }
+
+        }
+    }
+}
---
+++ HomeAdmin.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HomeAdmin.aspx.cs" Inherits="pruebitanetframework.HomeAdmin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <h1>Hola usuario ADMIN</h1>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
